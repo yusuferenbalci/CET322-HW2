8013f44e20f65b849527ee5daf3a7f404d443a3c
yusuferenbalci <56623398+yusuferenbalci@users.noreply.github.com>
2024-03-06 13:40:50 +0300

    homework done

---
+++ CetBooks/Controllers/BooksController.cs
@@ -0,0 +1,62 @@
+﻿using CetBooks.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CetBooks.Controllers
+{
+    public class BooksController : Controller
+    {
+        public IActionResult Index()
+        {
+            FakeDB fakeDB = new FakeDB();
+            var allbooks = fakeDB.GetAllBooks();
+
+            return View(allbooks);
+        }
+
+        public IActionResult Detail(int? id)
+        {
+            if (!id.HasValue) return BadRequest();
+
+            FakeDB db = new FakeDB();
+            var book = db.GetBookById(id.Value);
+
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            return View(book);
+
+        }
+
+        [HttpPost]
+        public IActionResult Create(Book book)
+        {
+            if (ModelState.IsValid)
+            {
+                FakeDB db = new FakeDB();
+                db.AddBook(book); 
+                return RedirectToAction("Index");
+            }
+            return View(book);
+        }
+
+
+        public IActionResult Delete(int? id)
+        {
+            if (!id.HasValue) return BadRequest();
+
+            FakeDB db = new FakeDB();
+            var result = db.DeleteBook(id.Value);
+            if (result)
+            {
+
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+    }
+}
